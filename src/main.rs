d46b9c9650838d47c3f7f8eb3eaf185014954e3a
Brendan <sydojiro@gmail.com>
2023-03-27 18:48:18 -0400

    Added tests, reworked main into more functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use std::{env, fs};
 
-fn get_cmd_args () -> (String, usize) {
-    // Get the command-line arguments and store them in a vector of strings
-    let args: Vec<String> = env::args().collect();
-
+fn get_cmd_args (args: Vec<String>) -> (String, usize) {
     // Extract the second command-line argument, which should be the path to the file to read
     let file_path = args[1].to_string();
 
@@ -12,6 +9,18 @@ fn get_cmd_args () -> (String, usize) {
 
     // Return `file_path` and `n` variables
     (file_path, n)
+}
+
+fn get_file_contents (file_path: String) -> String {
+    // Read the file's contents and store them in `contents` variable as a String.
+    // The `.expect()` line allows for error handling if the file is unable to be read for any reason
+    let contents: String = fs::read_to_string(file_path)
+        .expect("Unable to read file.");
+
+    // Print the contents of the file to the console
+    // println!("With text:\n{contents}");
+
+    contents
 }
 
 fn top_n_nums (mut numbers: Vec<i32>, n: usize) ->  () {
@@ -29,23 +38,21 @@ fn top_n_nums (mut numbers: Vec<i32>, n: usize) ->  () {
     for i in &nums_slice[..n] {
         println!("{}", i);
     }
+    
 }
 
 fn main() {
-    
+
+    // Get the command-line arguments and store them in a vector of strings
+    let args: Vec<String> = env::args().collect();
+
     // Assign `file_path` and `n` variables the return values from get_cmd_args
-    let (file_path, n): (String, usize) = get_cmd_args();
+    let (file_path, n): (String, usize) = get_cmd_args(args);
 
     // Print a message indicating the file that will be read
-    // println!("In file {}", file_path);
+    println!("In file {}", file_path);
 
-    // Read the file's contents and store them in `contents` variable as a String.
-    // The `.expect()` line allows for error handling if the file is unable to be read for any reason
-    let contents: String = fs::read_to_string(file_path)
-        .expect("Unable to read file.");
-
-    // Print the contents of the file to the console
-    // println!("With text:\n{contents}");
+    let contents: String = get_file_contents(file_path);
 
     // Initialize a mutable vector named `numbers` of type `Vec<i32>`
     let numbers: Vec<i32> = 
@@ -71,30 +78,49 @@ fn main() {
 
 }
 
+
+// Unit Tests
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // #[test]
-    // fn test_add() {
-    //     assert_eq!(add(1, 2), 3);
-    // }
-
-    // #[test]
-    // fn test_bad_add() {
-    //     // This assert would fire and test will fail.
-    //     // Please note, that private functions can be tested too!
-    //     assert_eq!(bad_add(1, 2), 3);
-    // }
-
     #[test]
-    fn zero_args() {
-        get_cmd_args();
+    fn get_args_zero_cmd_args() {
+        let args: Vec<String> = Vec::new();
+        get_cmd_args(args);
     }
 
-    // fn nonexistant_file() {
-    //     let no_file: String = String::("thisfiledoesnotexist");
-    //     get_cmd_args();
-    // }
+    #[test]
+    fn get_args_empty_cmd_args() {
+        let args: Vec<String> = vec![String::new(); 3];
+        get_cmd_args(args);
+    }
+
+    #[test]
+    fn get_args_wrong_order_args() {
+        let args: Vec<String> = vec![
+            String::from("path_of_executable"),
+            String::from("1"),
+            String::from("csv-file.csv")
+        ];
+        get_cmd_args(args);
+    }
+
+    #[test]
+    #[should_panic]
+    fn get_args_nonexistant_file() {
+        let args: Vec<String> = vec![
+            String::from("path_of_executable"),
+            String::from("thisfiledoesnotexist"),
+            String::from("1")
+        ];
+        get_cmd_args(args);
+    }
+
+    #[test]
+    fn get_contents_missing_file() {
+        let file_path: String = String::from("thisfiledoesnotexist");
+        get_file_contents(file_path);
+    }
 
 }
